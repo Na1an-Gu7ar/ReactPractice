538d22bd1e7f7221707290c16316a6033c3f3330
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-06-30 23:19:00 +0530

    Image Slder complete

--- src/Components/Image-slider/index.jsx
+++ src/Components/Image-slider/index.jsx
@@ -1,42 +1,47 @@
 import React, { useEffect, useState } from 'react'
 
 const index = () => {
-    const [page, setPage] = useState(0)
+    const [page, setPage] = useState(1)
     const [data, setData] = useState([])
 
-    let url = `https://picsum.photos/v2/list?page=${page}&limit=1`
     const fetchData = async () => {
+        let url = `https://picsum.photos/v2/list?page=${page}&limit=1`
         let fdata = await fetch(url)
         let parseData = await fdata.json()
-        setData(data.concat(parseData))
+        setData(parseData)
     }
 
     useEffect(() => {
         fetchData()
-    }, [url])
+    }, [])
 
     const nextSlide = async () => {
+        let url = `https://picsum.photos/v2/list?page=${page+1}&limit=1`
         setPage(page + 1)
+        let fdata = await fetch(url)
+        let parseData = await fdata.json()
+        setData(parseData)
     }
 
     const prevSlide = async () => {
+        let url = `https://picsum.photos/v2/list?page=${page-1}&limit=1`
         setPage(page - 1)
+        let fdata = await fetch(url)
+        let parseData = await fdata.json()
+        setData(parseData)
     }
 
     return (
         <div className='w-screen h-screen flex justify-center'>
-            {data && data.length ? data.map((d, index) => {
-                return (
-                    <div key={d.id} className="absolute card">
-                        <img className='w-80' src={`${d.download_url}`} />
-                    </div>
-                )
-
-            }) : null}
+            <div className="w-60 absolute card">
+                {data && data.length ? data.map((d, index) => (
+                    <img key={d.id} className={page != d.id ? 'absolute w-80 z-40' : 'hidden'} src={`${d.download_url}`} />
+                )) : null}
+            </div>
 
             <div className='flex justify-between gap-80 items-center h-56 z-30'>
-                <button onClick={() => nextSlide()} className='text-black text-2xl bg-white w-12 h-12 rounded-3xl'>&larr;</button>
-                <button onClick={() => prevSlide()} className='text-black text-2xl bg-white w-12 h-12 rounded-3xl'>&rarr;</button>
+                <button onClick={() => prevSlide()} className='text-black text-2xl bg-white w-12 h-12 rounded-3xl'>&larr;</button>
+                <button onClick={() => nextSlide()} className='text-black text-2xl bg-white w-12 h-12 rounded-3xl'>&rarr;</button>
             </div>
         </div>
     )
