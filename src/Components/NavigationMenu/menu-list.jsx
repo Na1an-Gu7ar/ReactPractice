c25d049d36a8ed7a7b8f5a80429cd921bccca373
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-07-02 15:28:54 +0530

    Chnages in Tree View

--- src/Components/NavigationMenu/menu-list.jsx
+++ src/Components/NavigationMenu/menu-list.jsx
@@ -4,7 +4,7 @@ import MenuItem from './menu-item'
 const MenuList = ({list = []}) => {
   return (
     <ul className='relative left-5'>
-      {list && list.length ? list.map((listitem) => <MenuItem item={listitem} />) : null}
+      {list && list.length ? list.map((listitem) => <MenuItem key={listitem.label} item={listitem} />) : null}
     </ul>
   )
 }
