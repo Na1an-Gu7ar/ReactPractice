627f748df8efe68347e3ad64b4c8425a1f84109e
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-07-05 17:11:44 +0530

    LightDark completed

--- src/Components/LightDark/index.jsx
+++ src/Components/LightDark/index.jsx
@@ -12,7 +12,7 @@ const index = () => {
 
     return (
         <div className={`${theme} w-screen h-screen transition ease-in-out duration-700`}>
-            <button onClick={() => handleTheme()} className={theme === 'bg-white' ? 'text-black' : 'text-white'}>Change</button>
+            <button onClick={() => handleTheme()} className={theme === 'bg-white' ? 'text-white bg-black rounded-md py-1.5 px-2.5' : 'text-black bg-white rounded-md py-1.5 px-2.5'}>Change</button>
         </div>
     )
 }
