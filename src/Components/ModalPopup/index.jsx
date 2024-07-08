be684e4ab2ca6d13924c28e74ccb0fb26cb0003f
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-07-09 00:02:03 +0530

    ModalPopup completed

--- src/Components/ModalPopup/index.jsx
+++ src/Components/ModalPopup/index.jsx
@@ -5,16 +5,16 @@ const index = () => {
     const [click, setClick] = useState('opacity-0 translate-y-0')
 
     const handleClick = () => {
-        setClick(click==='opacity-0 translate-y-0' ? 'opacity-100 translate-y-10' : 'opacity-0 translate-y-0')
+        setClick(click==='opacity-0 translate-y-0' ? 'opacity-100 translate-y-5' : 'opacity-0 translate-y-0')
     }
 
     return (
         <div className='w-screen h-screen flex flex-col justify-center items-center'>
-            <button onClick={() => handleClick()}>Click Me</button>
-            <div className={`${click} w-4/6 h-96 bg-blue-300 flex flex-col gap-5 items-center justify-center transition ease-in-out duration-1000`}>
-                <input type="text" placeholder='Name' />
-                <input type="text" placeholder='City' />
-                <input type="text" placeholder='State' />
+            <button onClick={() => handleClick()} className={click==='opacity-0 translate-y-0' ? '' : 'opacity-0'}>Click Me</button>
+            <div className={`${click} w-4/6 h-96 bg-blue-300 flex flex-col gap-5 items-center justify-center transition ease-in-out duration-500`}>
+                <div className='w-10 relative bottom-40' style={{left: '27rem'}}>
+                    <button onClick={() => handleClick()}>X</button>
+                </div>
             </div>
         </div>
     )
