4d226aa476a214dbd58c4cb6fd85c4aeea19da01
Na1an-Gu7ar <gujarnayan8@gmail.com>
2024-07-11 12:27:24 +0530

    GitHub profile finder completed

--- src/Components/Profile-finder/index.jsx
+++ src/Components/Profile-finder/index.jsx
@@ -4,18 +4,26 @@ const index = () => {
     const [username, setUsername] = useState('Na1an-Gu7ar')
     const [profiles, setProfiles] = useState([])
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
 
     // fetch(`https://api.github.com/users/${username}`)
 
     const loadData = async () => {
         // setLoading(true)
         try {
-            // setLoading(true)
+            setLoading(true)
             let url = `https://api.github.com/users/${username}`
             let response = await fetch(url)
-            let data = await response.json()
-            setProfiles([data])
+            if(response.ok){
+                let data = await response.json()
+                setProfiles([data])
+            }else{
+                setError('Profile Not Exist')
+            }
+
         } catch (err) {
+            setLoading(false)
+        } finally {
             setLoading(false)
         }
     }
@@ -24,37 +32,26 @@ const index = () => {
         loadData()
     }, [])
 
-    // console.log(profiles);
-
-    // const handleClick = (e) => {
-    //     setUsername(e.target.value)
-    //     loadData()
-    // }
-
-    if (loading) {
-        return <div>Loading...</div>
-    }
-
     return (
-        <div className='flex flex-col gap-10 justify-center items-center flex-shrink'>
-            <div>
-                <input type="text" className='border border-solid border-black' value={username} onChange={(e) => setUsername(e.target.value)} />
-                <button onClick={() => loadData()}>Load</button>
+        <div className='flex flex-col gap-10 justify-center items-center w-screen h-screen'>
+            <div className='flex flex-row gap-5'>
+                <input type="text" name={username} className='border border-solid border-black' placeholder='Enter Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <button onClick={() => loadData()} className='bg-gray-300 rounded-md w-16'>Search</button>
             </div>
-            {loading ? loading : profiles.map((p) => {
+            {error.length > 0 ? error : (loading ? <div>Loading...</div> : profiles.map((p) => {
                 return (
-                    <div key={p.id} className='flex flex-row gap-10 flex-wrap'>
-                        <div>Username: {p.login}</div>
-                        <div className='flex flex-row gap-10 flex-wrap'>
-                            <div className='w-40'><img src={p.avatar_url} alt={p.login} /></div>
+                    <div key={p.id}>
+                        <div className='flex flex-row gap-10 flex-wrap justify-start items-center' style={{ width: '12rem' }}>
+                            <div>Username: {p.login}</div>
+                            <div className='w-52'><img src={p.avatar_url} alt={p.login} /></div>
                             <div>Followers:<br />{p.followers}</div>
                             <div>Following:<br />{p.following}</div>
                             <div>Repositories:<br />{p.public_repos}</div>
-                            <div><a href={p.html_url} className='text-blue-500 hover:underline'>Visit</a></div>
+                            <div><a href={p.html_url} target='_blank' className='text-blue-500 hover:underline'>Visit</a></div>
                         </div>
                     </div>
                 )
-            })}
+            }))}
         </div>
     )
 }
